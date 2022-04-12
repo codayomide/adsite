e9de12d8db504cd194a9ae9d0b91862d7fd131b0
codayomide <smokayomide@gmail.com>
2022-04-12 23:21:44 +0100

    commit on 'main'

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const inputBtn = document.getElementById("input-btn");
+const inputBtn = document.getElementById("input-btn")
 
-const nameInput = document.getElementById("name-input");
-const imgInput = document.getElementById("img-input");
-const waNoInput = document.getElementById("wa-no-input");
+const nameInput = document.getElementById("name-input")
+const imgInput = document.getElementById("img-input")
+const waNoInput = document.getElementById("wa-no-input")
 
-let productList = [];
-const productsWrapper = document.getElementById("products-wrapper");
-let btnList;
-let counter = 0;
+let productList = []
+const productsWrapper = document.getElementById("products-wrapper")
+let btnList
+let counter = 0
+const productsFromLocalStorage = JSON.parse( localStorage.getItem("productList") )
 
-function listProduct() {
-  console.log("product");
+if (productsFromLocalStorage) {
+  productList = productsFromLocalStorage
+  render(productList)
 }
 
 function render(list) {
-  productsWrapper.innerHTML = "";
+  productsWrapper.innerHTML = ""
   list.forEach((product) => {
     productsWrapper.innerHTML += `
-            <div class="products">
-                <img src="${product[1]}">
-                <h1>${product[0]}</h1>
-                ${product[3]}
-               
-            </div>
-        `;
-  });
+        <div class="products">
+            <img src="${product[1]}">
+            <h1>${product[0]}</h1>
+            ${product[3]}
+          
+        </div>
+      `
+  })
 }
 
 inputBtn.addEventListener("click", () => {
@@ -33,12 +35,13 @@ inputBtn.addEventListener("click", () => {
     imgInput.value,
     waNoInput.value,
     `<a href='https://wa.me/${waNoInput.value}' target="_blank" id='btn${counter}' class='wa-btns'>${waNoInput.value}</a>`,
-  ]);
-  console.log(productList);
-  nameInput.value = "";
-  imgInput.value = "";
-  waNoInput.value = "";
+  ])
+  console.log(productList)
+  nameInput.value = ""
+  imgInput.value = ""
+  waNoInput.value = ""
+  localStorage.setItem("productList", JSON.stringify(productList) )
+  render(productList)
+  counter++
+})
 
-  render(productList);
-  counter++;
-});
